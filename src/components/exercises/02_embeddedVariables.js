94cdf133318009ecda38a39409d9ad6bdb4f3892
Mary Akastia Christo <n.akastia@gmail.com>
2022-09-30 13:29:35 +0100

    Exercise 2 completed.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -2,31 +2,47 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
+  
+    const modules = [
+      {
+        Name: "Devops",
+        NoLectures: 2,
+        NoPracticals: 2
+      },
+      {
+        Name: "Enterprise Web Dev",
+        NoLectures: 3,
+        NoPracticals: 2
+      }
+    ];
   return (
     <div>
-      <h1>Computer Sciences - Modules Table</h1>
+      <h1>HDip Computer Sciences - Modules Table</h1>
 
 
       <table className="table table-bordered">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>No lectures</th>
-          <th>No practicals</th>
-        </tr>
-      </thead>
-      <tbody >
-        <tr>
-          <td>DevOps</td>
-          <td>2</td>
-          <td>2</td>
-        </tr>
-        <tr>
-          <td>Enterprise Web Dev</td>
-          <td>3</td>
-          <td>2</td>
-        </tr>
-      </tbody >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].Name}</td>
+            <td>{modules[0].NoLectures}</td>
+            <td>{modules[0].NoPracticals}</td>
+          </tr>
+        
+          <tr>
+            
+            <td>{modules[1].Name}</td>
+            <td>{modules[1].NoLectures}</td>
+            <td>{modules[1].NoPracticals}</td>
+      
+          </tr>
+        </tbody>
     </table>
   </div>
   );
